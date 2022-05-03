2d616031df0ba0531b9cae03160964f895c4f942
cogroid <code@cogroid.com>
2022-05-03 15:56:44 -0400

    libunistring 2022-05-03 15:56

---
+++ version.sh
@@ -0,0 +1,6 @@
+# Version number and release date.
+VERSION_NUMBER=0.9.9
+RELEASE_DATE=2018-02-28      # in "date +%Y-%m-%d" format
+
+# Version of gnulib that was used in this release.
+GNULIB_GIT_COMMIT=e6aa578643640d094cbc4c0985066266203c1535
